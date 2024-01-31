a2e9a3ba45d9c5bd7c688f43cbdf8d21b6a9f4ae
AnitaPrzedwojewska <nisiek@wp.pl>
2024-01-31 14:27:09 +0100

    Added options to select

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,22 +1,36 @@
 import axios from 'axios';
 
-axios.defaults.headers.common['x-api-key'] =   'live_0sBujqVPlOO5f92mupcUKwJ9JNUfG0U4i9U1jXs5oImfnkLouJOawkIxXHqQBG34';
-
-const urlBreeds = `https://api.thecatapi.com/v1/breeds`;
+axios.defaults.headers.common['x-api-key'] =
+  'live_0sBujqVPlOO5f92mupcUKwJ9JNUfG0U4i9U1jXs5oImfnkLouJOawkIxXHqQBG34';
 
 export const fetchBreeds = () => {
+  const urlBreeds = `https://api.thecatapi.com/v1/breeds`;
   return fetch(urlBreeds)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    const result = data.map(({ id, name }) => ({ [id]: name }));
-    return result;
-  })
-  .catch(function (error) {
-   console.log(error);
-  });
-}
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(breeds => {
+      // console.log("fetchBreeds filtering starting:");
+      return breeds.map(({ id, name }) => ({ id, name }));
+    })
+    .catch(error => {
+      return error;
+    });
+};
+
+export const fetchCatByBreed = breedId => {
+  const urlBreed = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  return fetch(urlBreed)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      return error;
+    })
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import { fetchBreeds } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 // import SlimSelect from 'slim-select';
 
 // new SlimSelect({
 //   select: '#selectElement',
 // })
 
-let catBreeds = fetchBreeds();
-
 const selectElement = document.querySelector('.breed-select');
 
-console.log(catBreeds);
-// for (let i = 0; i < catBreeds.length; i++) {
-//   console.log(`Id: ${catBreeds[i].id}; Nazwa: ${catBreeds[i].name}`);
-// }
+const catBreedsOptions = fetchBreeds().then(breeds => {
+  console.log("breeds options:");
+  console.log(breeds);
+  // let options = '';
+  const selectOptions = breeds.map(
+    ({ id, name }) => `<option value="${id}">${name}</option>`).join('');
+  selectElement.insertAdjacentHTML('beforeend', selectOptions);
+  });
