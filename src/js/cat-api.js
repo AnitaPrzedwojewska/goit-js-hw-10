f882ebb95f0ea23e5ffea0754e4678dc91897029
AnitaPrzedwojewska <nisiek@wp.pl>
2024-02-04 16:27:43 +0100

    Added slim-select, axios, notiflix and styles

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,47 +1,66 @@
 import axios from 'axios';
 
-// axios.defaults.headers.common['x-api-key'] =   'live_0sBujqVPlOO5f92mupcUKwJ9JNUfG0U4i9U1jXs5oImfnkLouJOawkIxXHqQBG34';
 const apiKey =
   'live_0sBujqVPlOO5f92mupcUKwJ9JNUfG0U4i9U1jXs5oImfnkLouJOawkIxXHqQBG34';
+axios.defaults.headers.common['x-api-key'] = apiKey;
 
 export const fetchBreeds = () => {
   const urlBreeds = `https://api.thecatapi.com/v1/breeds?key_api=${apiKey}`;
-  return fetch(urlBreeds)
+  return axios(urlBreeds)
     .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(breeds => {
-      // console.log("fetchBreeds filtering starting:");
-      return breeds.map(({ id, name }) => ({ id, name }));
+      return { status: response.status, data: response.data };
     })
     .catch(error => {
-      return error;
+      return handleError(error);
     });
 };
 
 export const fetchCatByBreed = breedId => {
-  const urlBreed = `https://api.thecatapi.com/v1/images/search?api_key=${apiKey}&breed_ids=${breedId}`;
-  return fetch(urlBreed)
+  console.log("breedId: ", breedId);
+  const urlBreedInfo = `https://api.thecatapi.com/v1/images/search?api_key=${apiKey}&breed_ids=${breedId}`;
+  return axios(urlBreedInfo)
     .then((response) => {
-      if (!response.ok) {
-        console.log(response.status);
-        throw new Error(response.status);
-      }
-      return response.json();
+      console.log("response: ", response);
+      console.log("response.data[0]: ", response.data[0]);
+      return { status: response.status, data: response.data[0] };
     })
-    .then((catObject) => {
-      return {
-        name: catObject[0].breeds[0].name,
-        description: catObject[0].breeds[0].description,
-        temperament: catObject[0].breeds[0].temperament,
-        imgUrl: catObject[0].url
-      };
-    })
-    .catch(error => {
-      console.log(error);
-      return error;
-    })
+    .catch((error) => {
+      return handleError(error);
+    });
+};
+
+// .then(catObject => {
+//       return {
+//         name: catObject[0].breeds[0].name,
+//         description: catObject[0].breeds[0].description,
+//         temperament: catObject[0].breeds[0].temperament,
+//         imgUrl: catObject[0].url,
+//       };
+//     })
+
+const handleError = error => {
+  // debugger;
+  if (error.response) {
+    // console.log('error.response: ', error.response);
+    // console.log('error.response.status: ', error.response.status);
+    // console.log('error.response.data: ', error.response.data.message);
+    return {
+      status: error.response.status,
+      data: error.response.data.message,
+    };
+  } else if (error.request) {
+    return {
+      status: error.request.response.status,
+      data: error.request.response.data.message,
+    };
+  } else {
+    return {
+      status: error.status,
+      data: error.message,
+    };
+  }
+  return {
+    status: error.status,
+    data: error.config,
+  };
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,71 +1,92 @@
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import Notiflix from 'notiflix';
+
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-const loadingElement = document.querySelector('.loader');
+const selectElement = document.querySelector('#selectElement');
+const loadingElement = document.querySelector('.loader-container');
 const errorElement = document.querySelector('.error');
 const catInfoElement = document.querySelector('.cat-info');
-const selectElement = document.querySelector('.breed-select');
 
+selectElement.style.display = 'none';
 errorElement.style.color = 'red';
 errorElement.classList.add('visually-hidden');
+catInfoElement.style.display = 'none';
 
-// działa, ale niżej próba z biblioteką slim-select
 fetchBreeds()
+  .then(response => {
+    console.log(response);
+    if (response.status !== 200) {
+      errorElement.textContent = response.data;
+      loadingElement.style.display = 'none';
+      errorElement.style.display = 'block';
+      Notiflix.Notify.warning(response.data);
+      return;
+    }
+    loadingElement.style.display = 'block';
+    console.log('response.data: ', response.data);
+    return response.data.map(({ id, name }) => ({ id, name }));
+  })
   .then(breeds => {
-    loadingElement.classList.remove('visually-hidden');
     return breeds
       .map(({ id, name }) => `<option value="${id}">${name}</option>`)
       .join('');
   })
   .then(options => {
-    options = `<option value="">-- Select breed of cat --</option>${options}`;
+    selectElement.style.display = 'block';
+    options = `<option value="" data-placeholder="true"></option>${options}`;
     selectElement.insertAdjacentHTML('beforeend', options);
-    loadingElement.classList.add('visually-hidden');
+    loadingElement.style.display = 'none';
+    new SlimSelect({
+      select: '#selectElement',
+      settings: {
+        placeholderText: '-- Select breed of cat --',
+        contentLocation: document.getElementById('selectLocation'),
+      },
+    });
   })
   .catch(error => {
     console.log(error);
   });
 
-// import SlimSelect from 'slim-select';
-
-// fetchBreeds()
-//   .then(breeds => {
-//     return breeds.map(
-//       ({ id, name }) => `<option value="${id}">${name}</option>`).join('');
-//   })
-//   .then(options => {
-//     options = `<option value="">-- Select breed of cat --</option>${options}`;
-//     selectElement.insertAdjacentHTML('beforeend', options);
-//   })
-//   .then(() => {
-//     new SlimSelect({
-//       select: '#selectElement',
-//       settings: {
-//         placeholderText: '-- Select breed of cat --',
-//         contentLocation: document.getElementById('selectLocation'),
-//       },
-//     });
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-
 selectElement.addEventListener('change', event => {
-  loadingElement.classList.remove('visually-hidden');
+  // debugger;
+  loadingElement.style.display = 'block';
   const idBreed = event.target.value;
   console.log(idBreed);
-  fetchCatByBreed(idBreed).then(catInfo => {
-    console.log(catInfo);
-    catInfoElement.textContent = '';
-    const contentCatInfo = `<div class="cat-img">
-        <img src="${catInfo.imgUrl}" >
-      </div>
-      <div class="cat-text">
-        <h2 class="breed-name">${catInfo.name}</h2>
-        <p class="breed-description">${catInfo.description}</p>
-        <p class="breed-temperament"><b>Temperament:</b><br>${catInfo.temperament}</p>
-      </div>
-      `;
-    catInfoElement.insertAdjacentHTML('beforeend', contentCatInfo);
-    loadingElement.classList.add('visually-hidden');
-  });
+  fetchCatByBreed(idBreed)
+    .then(response => {
+      if (response.status !== 200) {
+        errorElement.textContent = response.data;
+        loadingElement.style.display = 'none';
+        errorElement.style.display = 'block';
+        Notiflix.Notify.warning(response.data);
+        return;
+      }
+      loadingElement.style.display = 'block';
+      console.log('response.data: ', response.data);
+      return {
+        name: response.data.breeds[0].name,
+        description: response.data.breeds[0].description,
+        temperament: response.data.breeds[0].temperament,
+        imgUrl: response.data.url,
+      };
+    })
+    .then(catInfo => {
+      console.log('catInfo:', catInfo);
+      catInfoElement.style.display = 'flex';
+      catInfoElement.textContent = '';
+      const contentCatInfo = `<div class="cat-img">
+      <img src="${catInfo.imgUrl}" >
+    </div>
+    <div class="cat-text">
+      <h2 class="breed-name">${catInfo.name}</h2>
+      <p class="breed-description">${catInfo.description}</p>
+      <p class="breed-temperament"><b>Temperament:</b><br>${catInfo.temperament}</p>
+    </div>
+    `;
+      catInfoElement.insertAdjacentHTML('beforeend', contentCatInfo);
+      loadingElement.style.display = 'none';
+    });
 });
